91f829e1b2069f2d30d3d728b26d77aa2b246306
gorka-es <ggoorrkkiiggmm@gmail.com>
2024-10-08 15:50:23 +0200

    Actualizado el archivo gitignore y eliminada la carpeta Nueva carpeta

--- Nueva carpeta/js/config.js
+++
@@ -1,9 +0,0 @@
-// config.js
-export const config = {
-    url: 'https://sede.inap.gob.es/intervencion-tesoreria-entrada-acceso-libre-2020-2021-2022', // Cambia esta URL por la que desees monitorear
-    className: 'journal-content-article', // Cambia esto por la clase que deseas comparar
-    messages: {
-        changesDetected: "Ha habido cambios en el contenido.",
-        noChanges: "No ha habido cambios en el contenido."
-    }
-};
